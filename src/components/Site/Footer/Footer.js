c714f62cff635b0709f12337181d781589d05f5d
Scott Riley <hugs@scottriley.im>
2016-03-23 23:07:53 +0000

    Footer stuff.

--- src/components/Site/Footer/Footer.js
+++ src/components/Site/Footer/Footer.js
@@ -10,14 +10,19 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className={s.root}>
-        <Wrapper>
+        <div className={s.wrap}>
           <div className={s.getInTouch}>
             <Link to="making-stuff/for-you" className={s.cta}>Get in touch</Link>
           </div>
-          <p className={s.copyNotice}>
-            &copy; Forever and ever until the end of time
-          </p>
-        </Wrapper>
+          <div>
+            <p className={s.copyNotice}>
+              &copy; Forever and ever until the end of time
+            </p>
+            <p className={s.nav}>
+              <Link to="/" className={[s.link].join(' ')}>Home</Link> / <Link to="/" className={[s.link].join(' ')}>About</Link> / <Link to="/" className={[s.link].join(' ')}>What I do</Link> / <Link to="/" className={[s.link].join(' ')}>My work</Link> / <Link to="/" className={[s.link].join(' ')}>Speaking</Link>
+            </p>
+          </div>
+        </div>
       </footer>
     )
   }
